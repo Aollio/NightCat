b3c8096dc3fb8f069f06db29e64b36196bf8bb98
Ticknick <www.houyudong@qq.com>
2017-07-07 09:37:31 +0800

    tail

---
+++ src/pages/designer/me/me.ts
@@ -0,0 +1,16 @@
+
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+@Component({
+    selector:'page-me-designer',
+    templateUrl:'me.html'
+})
+export class DesignerMePage{
+
+    constructor(public navCtrl:NavController){
+
+    }
+
+
+
+}
