a87df02258475651098ad555d2e5fa0658fe39f4
Aollio <aollio@outlook.com>
2017-07-14 14:12:43 +0800

    home-

---
+++ src/pages/employer/projects/projects.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'page-employer-projects',
+    templateUrl: 'projects.html'
+})
+export class ProjectsPage {
+
+    constructor(){}
+
+}
