73945b2de3c7397dc676aaefa62218f25721c0a9
Ticknick <www.houyudong@qq.com>
2017-07-14 13:43:07 +0800

    8848848844848484848

--- src/pages/employer/helpchoose/helpchoose.ts
+++ src/pages/employer/helpchoose/helpchoose.ts
@@ -2,10 +2,53 @@ import {Component} from '@angular/core';
 
 @Component({
     templateUrl:'helpchoose.html',
-    selector:'Page-helpchoose'
+    selector:'page-helpchoose'
 })
 export class HelpChoosePage{
 constructor(){}
 state=1;
-
+    typegroup: Array<Array<{ name, imageurl }>> = [
+        [
+            {
+                name: '建筑工程1', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程2', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程3', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程4', imageurl: 'assets/img/first.png'
+            },
+        ],
+        [
+            {
+                name: '建筑工程12', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程12', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程23', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程12', imageurl: 'assets/img/first.png'
+            },
+        ],
+        [
+            {
+                name: '建筑工程1', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程12', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程12', imageurl: 'assets/img/first.png'
+            },
+            {
+                name: '建筑工程43', imageurl: 'assets/img/first.png'
+            },
+        ],
+    ]
 }
