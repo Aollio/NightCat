d3828f2f03ac8ccd2f8f3dbfb8cd8958c3a829c7
Ticknick <www.houyudong@qq.com>
2017-07-17 14:37:59 +0800

    8848848844848484848

--- src/pages/designer/publishtask/publishtask.ts
+++ src/pages/designer/publishtask/publishtask.ts
@@ -7,6 +7,7 @@ import {Component} from "@angular/core";
 export class PublishTaskPage {
     public event = {
         timeStarts: '2017-01-01',
-        timeEnds: '2017-01-01'
+        timeEnds: '2017-01-01',
+        timeEnds1: '2017-01-01'
     }
 }
