2b3e12cd17d5b7706f531b1aac9d3568d25662b6
Ticknick <www.houyudong@qq.com>
2017-07-17 18:35:07 +0800

    8848848844848484848

--- src/pages/employer/home/home.ts
+++ src/pages/employer/home/home.ts
@@ -16,50 +16,31 @@ export class EmployerHomePage {
         {url: 'assets/img/home-default.png'},
     ]
 
-    typegroup: Array<Array<{ name, imageurl }>> = [
-        [
-            {
-                name: '建筑工程1', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程2', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程3', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程4', imageurl: 'assets/img/first.png'
-            },
-        ],
-        [
-            {
-                name: '建筑工程12', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程12', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程23', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程12', imageurl: 'assets/img/first.png'
-            },
-        ],
-        [
-            {
-                name: '建筑工程1', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程12', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程12', imageurl: 'assets/img/first.png'
-            },
-            {
-                name: '建筑工程43', imageurl: 'assets/img/first.png'
-            },
-        ],
+    types: Array<{ name, icon }> = [
+        {
+            name: '建筑工程1', icon: 'ios-list-box-outline'
+        },
+        {
+            name: '建筑工程2', icon: 'ios-list-box-outline'
+        },
+        {
+            name: '建筑工程3', icon: 'add'
+        },
+        {
+            name: '建筑工程4', icon: 'close'
+        },
+        {
+            name: '建筑工程12', icon: 'add'
+        },
+        {
+            name: '建筑工程12', icon: 'ios-calendar-outline'
+        },
+        {
+            name: '建筑工程23', icon: 'ios-briefcase-outline'
+        }
+
     ]
+
 
     desarray:Array<{avatar,name,time,describe}> = [
         {
