b0acf7093aaf655027fe906337223bbf5fb520fc
Aollio <aollio@outlook.com>
2017-07-19 11:13:40 +0800

    almost

--- src/pages/employer/pay/pay.ts
+++ src/pages/employer/pay/pay.ts
@@ -1,8 +1,25 @@
 import {Component} from "@angular/core";
-@Component({
-    templateUrl:'pay.html',
-    selector:'page-pay'
-})
-export class PayPage{
 
+@Component({
+    templateUrl: 'pay.html',
+    selector: 'page-pay'
+})
+export class PayPage {
+
+
+    state: number = 3
+
+    pay: string;
+
+    payment(way) {
+        this.pay = way
+    }
+
+    comfirm_payment() {
+        this.state = 3
+    }
+
+    comfirm_paymoney(){
+        this.state = 4
+    }
 }
