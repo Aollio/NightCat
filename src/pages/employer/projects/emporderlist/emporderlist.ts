80691390c4b2f68aaaa07c295c83cfd768fc0647
Aollio <aollio@outlook.com>
2017-07-19 11:56:50 +0800

    almost

--- src/pages/employer/projects/emporderlist/emporderlist.ts
+++ src/pages/employer/projects/emporderlist/emporderlist.ts
@@ -1,9 +1,28 @@
 import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {PublishTaskMiddleWarePage} from "../../publish-task-middleware/publish-task-middleware";
 
 @Component({
     selector: 'com-emporderlist',
-    templateUrl:'emporderlist.html'
+    templateUrl: 'emporderlist.html'
 })
-export class EmpOrderListComponent{
-    constructor(){}
+export class EmpOrderListComponent {
+
+    orderlist: Array<{}> = [];
+
+    constructor(public nav:NavController) {
+    }
+
+
+    isEmpty() {
+        return this.orderlist.length === 0
+    }
+
+    openFindDesigner(){
+        this.nav.parent.select(0)
+    }
+
+    openPulishTask(){
+        this.nav.parent.select(2)
+    }
 }
