d729e0edc075fa4f35147be7f6c93b5b29ee78d3
icesnowy <333666999>
2017-07-24 18:48:54 +0800

    嘿嘿嘿

--- src/pages/designer/me/modifyprofile/modifyprofile.ts
+++ src/pages/designer/me/modifyprofile/modifyprofile.ts
@@ -21,19 +21,19 @@ export class ModifyProfilePage{
 
     desclassarray: Array<{val,name}> =[
 
-        {name: '概预算', val: '0'},
-        {name: '项目经理', val: '1'},
-        {name: '审图', val: '2'},
-        {name: '策划', val: '3'},
-        {name: '规划设计', val: '4'},
-        {name: '建筑设计', val: '5'},
-        {name: '电气设计', val: '6'},
-        {name: '结构设计', val: '7'},
-        {name: '软装设计', val: '8'},
-        {name: '给排水设计', val: '9'},
-        {name: '室内设计', val: '10'},
-        {name: '暖通设计', val: '11'},
-        {name: '景观设计', val: '12'}
+        {'name': '概预算', 'val': '0'},
+        {'name': '项目经理', 'val': '1'},
+            {'name': '审图', 'val': '2'},
+            {'name': '策划', 'val': '3'},
+            {'name': '规划设计', 'val': '4'},
+            {'name': '建筑设计', 'val': '5'},
+            {'name': '电气设计', 'val': '6'},
+            {'name': '结构设计', 'val': '7'},
+            {'name': '软装设计', 'val': '8'},
+            {'name': '给排水设计', 'val': '9'},
+            {'name': '室内设计', 'val': '10'},
+            {'name': '暖通设计', 'val': '11'},
+            {'name': '景观设计', 'val': '12'}
 
 
     ];
