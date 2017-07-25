de11c3a9ab48f03de5a067e1facda55c94490f62
icesnowy <333666999>
2017-07-25 14:46:18 +0800

    blank,tesk

---
+++ src/js/cat.js
@@ -0,0 +1,6 @@
+function initializeFontSize(){
+    var html=document.documentElement;
+    var width=html.getBoundingClientRect().width;
+    html.style.fontSize=width/16+'px';
+    console.log('This is nightcat')
+}
--- src/pages/blank/blank.ts
+++ src/pages/blank/blank.ts
@@ -1,9 +1,22 @@
 import {Component} from "@angular/core";
 
+
+declare let initializeFontSize:any
+
 @Component({
     selector:'page-blank',
     templateUrl: 'blank.html'
 })
 export class BlankPage {
     constructor(){}
+
+    images: Array<{ url }> = [
+        {url: 'assets/img/home-default.png'},
+        {url: 'assets/img/home-default.png'},
+        {url: 'assets/img/home-default.png'},
+    ]
+
+    ionViewDidEnter(){
+        initializeFontSize()
+    }
 }
