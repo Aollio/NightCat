7b07f58851790f701ed0b3633a506f427eee9af0
icesnowy <333666999>
2017-07-29 11:57:07 +0800

    employer me

--- src/pages/employer/me/me.ts
+++ src/pages/employer/me/me.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {NavController} from "ionic-angular";
 import {EmpFavoriteDesignerPage} from "./favorite-designer/favorite-designer";
 import {SharedService} from "../../../service/share.service";
+declare let initializeFontSize: any;
 
 @Component({
     selector: 'page-me-employer',
@@ -12,7 +13,9 @@ export class EmployerMePage {
     follower: EmpFavoriteDesignerPage;
 
     user;
-
+    ionViewDidEnter() {
+        initializeFontSize()
+    }
     constructor(public  navCtrl: NavController, public share: SharedService) {
         this.user = share.currentUser
     }
