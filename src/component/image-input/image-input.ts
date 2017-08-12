3a489eab2810e845003ef6adf548a71b17f4e464
Aollio <aollio@outlook.com>
2017-08-12 17:51:21 +0800

    fix

---
+++ src/component/image-input/image-input.ts
@@ -0,0 +1,76 @@
+import {Component, forwardRef} from '@angular/core';
+
+import {NavController} from "ionic-angular";
+import {ImageService} from "../../service/ajax/imgs.service";
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+
+
+const noop = () => {
+};
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => ImageInputComponent),
+    multi: true
+};
+
+
+@Component({
+    selector: 'image-input',
+    templateUrl: 'image-input.html',
+    providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
+})
+export class ImageInputComponent implements ControlValueAccessor {
+
+
+    private imgs;
+
+
+    constructor(private nav: NavController,
+                private imagesServ: ImageService) {
+    }
+
+    uploading_img() {
+
+        this.imagesServ.picker({})
+            .then(result => {
+                console.log("async imagesServ: ")
+                console.log(result)
+                for (let x of result) {
+                    console.log("imagesServe:")
+                    console.log(x)
+                    this.imgs.push(x)
+                }
+            })
+            .catch(error => console.log(error))
+        this.imgs.push("http://yemaotest.shanghai-cu.com/files/15025091100358078.jpg")
+    }
+
+
+    private onTouchedCallback: () => void = noop;
+    private onChangeCallback: (_: any) => void = noop;
+
+
+    //Set touched on blur
+    onBlur() {
+        this.onTouchedCallback();
+    }
+
+    //From ControlValueAccessor interface
+    writeValue(v: any) {
+        this.imgs = v;
+        this.onChangeCallback(v);
+    }
+
+    //From ControlValueAccessor interface
+    registerOnChange(fn: any) {
+        this.onChangeCallback = fn;
+    }
+
+    //From ControlValueAccessor interface
+    registerOnTouched(fn: any) {
+        this.onTouchedCallback = fn;
+    }
+
+
+}
---
+++ src/service/ajax/imgs.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+import {SharedService} from "../share.service";
+import {KeynoteService} from "../keynote.service";
+import {FileService} from "./files.service";
+import {ImagePicker} from "@ionic-native/image-picker";
+
+@Injectable()
+export class ImageService {
+
+
+    constructor(public shared: SharedService,
+                public keynote: KeynoteService,
+                private imagePicker: ImagePicker,
+                private files: FileService) {
+    }
+
+
+    public async picker(options = {}) {
+        options = options || {
+            maximumImagesCount: 15,
+            quality: 100,
+            outputType: 1
+        }
+        options["outputType"] = 1
+
+        console.log("开始选择图片")
+
+        let result = []
+
+        let imgs = await this.imagePicker.getPictures(options)
+
+        for (let i = 0; i < imgs.length; i++) {
+            console.log("选择图片后")
+            let url = await this.files.upload(imgs[i])
+            result.push(url)
+        }
+
+        console.log("picker: return urls")
+        console.log(result)
+        return result;
+    }
+
+
+}
